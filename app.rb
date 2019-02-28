80b166a1116cf32bf7ebef8008e13e3024f38185
itggot-christoffer-polndorfer-oresten <christoffer.polndorferoresten@itggot.se>
2019-02-28 10:57:32 +0100

    inlogg och redigering av profil funkar typ...

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require'sqlite3'
 require'sinatra'
 require'byebug'
 require'BCrypt'
+
 enable :sessions
 
 get('/') do #main sidan
@@ -22,7 +23,7 @@ post('/creating_acc') do
     db.results_as_hash = true
     #Kryptera lösen
     hashed_password = BCrypt::Password.create(params["password"])
-    db.execute("INSERT INTO users(Username, Password) VAlUES(?, ?)", params["name"], hashed_password)
+    db.execute("INSERT INTO users(Username, Password, Email, Phone) VALUES(?, ?, ?, ?)", params["name"], hashed_password, params["email"], params["phone"])
     #Redirecta till första sidan
     session[:remove_login] = nil
     redirect('/')
@@ -31,9 +32,10 @@ end
 post('/login') do
     db = SQLite3::Database.new("db/blogg_db.db")
     db.results_as_hash = true
-    result = db.execute("SELECT Username, Password FROM users WHERE users.Username = ?", params["name"])
+    result = db.execute("SELECT Username, Password, UserId FROM users WHERE users.Username = ?", params["name"])
     if result.length > 0 && BCrypt::Password.new(result.first["Password"]) == params["password"]
         session[:name] = result[0]["Username"]
+        session[:id] = result[0]["UserId"]
         session[:loggedin?] = true
         redirect('/')
     else
@@ -68,3 +70,35 @@ post('/submit_post') do
     db.execute("INSERT INTO posts (PostText) VALUES (?)", params["post"])
     redirect('/')
 end 
+
+post('/edit_post') do 
+    post_id = params["PostId"]
+    redirect("/edit_post/#{post_id}")
+end 
+
+get('/edit_post/:postid') do 
+    if session[:loggedin?] == true 
+        db = SQLite3::Database.new("db/blogg_db.db")
+        db.results_as_hash = true
+        post_info = db.execute("SELECT PostText FROM posts WHERE PostId = ?", params["PostId"])
+        slim(:edit_post, locals:{post_info: post_info})
+    else
+        redirect('/')
+    end 
+end 
+
+post('/updating') do 
+    db = SQLite3::Database.new("db/blogg_db.db")
+    db.results_as_hash = true
+    db.execute("UPDATE users SET Username = ?, Password = ?, Email = ?, Phone = ? WHERE UserId = ?", params["name"], params["password"], params["email"], params["phone"], session[:id])
+
+    redirect('/')
+end 
+ 
+
+# post('/deleting_post') do 
+#     db = SQLite3::Database.new("db/blogg_db.db")
+#     db.results_as_hash = true
+#     db.execute("DELETE FROM posts WHERE PostId = ?", params[""])
+
+# end 
